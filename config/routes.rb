03609e922f108d551e35746e12c9b2f456c5ffa5
Raghu Betina <raghu@firstdraft.com>
2019-12-09 03:50:46 +0000

    2nd draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,43 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+  root :to => "flights#index"
+  # Routes for the User resource:
+
+  # CREATE
+  get("/users/new", { :controller => "users", :action => "new_form" })
+  post("/create_user", { :controller => "users", :action => "create_row" })
+
+  # READ
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:id_to_display", { :controller => "users", :action => "show" })
+
+  # UPDATE
+  get("/users/:prefill_with_id/edit", { :controller => "users", :action => "edit_form" })
+  post("/update_user/:id_to_modify", { :controller => "users", :action => "update_row" })
+
+  # DELETE
+  get("/delete_user/:id_to_remove", { :controller => "users", :action => "destroy_row" })
+
+  #------------------------------
+
+  # Routes for the Flight resource:
+
+  # CREATE
+  get("/flights/new", { :controller => "flights", :action => "new_form" })
+  post("/create_flight", { :controller => "flights", :action => "create_row" })
+
+  # READ
+  get("/flights", { :controller => "flights", :action => "index" })
+  get("/flights/:id_to_display", { :controller => "flights", :action => "show" })
+
+  # UPDATE
+  get("/flights/:prefill_with_id/edit", { :controller => "flights", :action => "edit_form" })
+  post("/update_flight/:id_to_modify", { :controller => "flights", :action => "update_row" })
+
+  # DELETE
+  get("/delete_flight/:id_to_remove", { :controller => "flights", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
